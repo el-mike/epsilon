46ac1af68463bc7a1dd867c6aefab61e7dc4eb34
el-mike <michal.huras5@gmail.com>
2023-07-10 01:21:36 +0200

    feat: simple move test

---
+++ engine/src/common/algebraic_coord.rs
@@ -0,0 +1,46 @@
+use crate::common::coord::Coord;
+
+pub struct AlgebraicCoord {
+    value: String,
+}
+
+const ASCII_A_CODE: i8 = 97;
+
+impl AlgebraicCoord {
+    pub fn from_string(algebraic_coord: &str) -> Self {
+        let chars: Vec<char> = algebraic_coord.chars().collect();
+
+        if chars.len() != 2 {
+            panic!("Incorrect algebraic coord: {}", algebraic_coord);
+        }
+
+        return Self { value: String::from(algebraic_coord) };
+    }
+
+    pub fn from_coord(coord: &Coord) -> Self {
+        let rank = char::from(coord.y as u8);
+
+        let ascii_file = ASCII_A_CODE + coord.x;
+        let file = (ascii_file as u8) as char;
+
+        return Self {
+            value: format!("{file}{rank}"),
+        }
+    }
+
+    pub fn to_coord(&self) -> Coord {
+        let chars: Vec<char> = self.value.chars().collect();
+
+        if chars.len() != 2 {
+            panic!("Malformed algebraic coord: {}", self.value);
+        }
+
+        let file = (chars[0] as u32) - ASCII_A_CODE as u32;
+        let rank = chars[1].to_digit(10).unwrap();
+
+        return Coord {
+            x: file as i8,
+            y: rank as i8,
+        }
+    }
+}
--- engine/src/main.rs
+++ engine/src/main.rs
@@ -10,9 +10,20 @@ mod position;
 
 use crate::fen::parser;
 use crate::board::board::Board;
+use crate::common::coord::Coord;
+use crate::moves::piece_move::PieceMove;
 
 fn main() {
     let mut board = parser::parse(common::constants::FEN_STARTING_POSITION);
+
+    println!("{}", board);
+
+    let source = Coord::new(4, 1);
+    let target = Coord::new(4, 3);
+
+    let mut piece_move = PieceMove::new(source, target, false);
+
+    board.make_move(&mut piece_move);
 
     println!("{}", board);
 }
